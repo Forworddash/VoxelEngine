335104f6d654d75f5760b41ec495cc841795366b
samuel <samuel.baker@rack-a-tiers.com>
2024-03-03 22:11:49 -0800

    [CHG] changed default player settings and chunk size

--- settings.py
+++ settings.py
@@ -7,14 +7,14 @@ import math
 WIN_RES = glm.vec2(1600, 900)
 
 # chunk
-CHUNK_SIZE = 32
+CHUNK_SIZE = 64
 H_CHUNK_SIZE = CHUNK_SIZE // 2
 CHUNK_AREA = CHUNK_SIZE * CHUNK_SIZE
 CHUNK_VOL = CHUNK_AREA * CHUNK_SIZE
 
 # camera
 ASPECT_RATIO = WIN_RES.x / WIN_RES.y
-FOV_DEG = 50
+FOV_DEG = 90
 V_FOV = glm.radians(FOV_DEG)  # vertical FOV
 H_FOV = 2 * math.atan(math.tan(V_FOV * 0.5) * ASPECT_RATIO)  # horizontal FOV
 NEAR = 0.1
@@ -22,7 +22,7 @@ FAR = 2000.0
 PITCH_MAX = glm.radians(89)
 
 # player
-PLAYER_SPEED = 0.005
+PLAYER_SPEED = 0.010
 PLAYER_ROT_SPEED = 0.003
 PLAYER_POS = glm.vec3(H_CHUNK_SIZE, CHUNK_SIZE, 1.5 * CHUNK_SIZE)
 MOUSE_SENSITIVITY = 0.002
